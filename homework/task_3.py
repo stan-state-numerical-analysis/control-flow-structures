bc85cac5f667248ec438804f5bdeb0d4bd378ee8
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 08:46:44 -0700

    Update task_3.py

--- homework/task_3.py
+++ homework/task_3.py
@@ -1,4 +1,4 @@
-# Task: Create functions that approximate mathematical functions using their Maclaurin series and while loops.
+# Task: Create functions that approximate mathematical functions using their Taylor series expansion and while loops.
 
 # 1. Define a function called approximate_euler_number that takes a tolerance tol as input and outputs the
 # approximate value of Euler's number e using the Maclaurin series of e^x.
@@ -6,7 +6,7 @@
 
 
 # 2. Define a function called approximate_ln that takes a number x (0 < x <= 2) and a tolerance tol as input
-# and outputs the approximate natural logarithm of x using the Maclaurin series of ln(x).
+# and outputs the approximate natural logarithm of x using the Taylor series of ln(x) centered at 1.
 
 
 
