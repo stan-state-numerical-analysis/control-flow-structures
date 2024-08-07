9f63f2aca1ff5f30ed364ded7e1a7c97faf68362
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 20:06:33 -0700

    Update task_2.py

--- homework/task_2.py
+++ homework/task_2.py
@@ -1,1 +1,19 @@
+# Task: Create functions using for loops.
 
+# 1. Define a function called product_list that takes a list of numbers as input and outputs the product of all the numbers in the list.
+
+
+
+# 2. Define a function called sum_of_squares that takes a positive integer n as input and outputs the sum of the squares of all numbers from 1 to n.
+
+
+
+# 3. Define a function called factorial that takes a positive integer n as input and outputs the factorial of n.
+
+
+
+# 4. Define a function called arithmetic_series that takes three integers, a, d, and n, and outputs the sum of the first n terms of the arithmetic series with initial term a and common difference d.
+
+
+
+# 5. Define a function called geometric_series that takes three integers, a, r, and n, and outputs the sum of the first n terms of the geometric series with initial term a and common ratio r.
