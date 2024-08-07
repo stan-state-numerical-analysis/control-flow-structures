557217bc7588a49705eca7b284d898ce398e6947
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 19:54:20 -0700

    Update __init__.py

--- homework/__init__.py
+++ homework/__init__.py
@@ -1,1 +1,11 @@
+# Task: Create functions using conditional statements.
 
+# 1. Define a function called is_even that takes an integer input and outputs the boolean value True if the number is even, and False otherwise.
+
+# 2. Define a function called max_of_two that takes two numerical inputs and outputs the maximum value.
+# Do not use a built-in function here, instead use a conditional statement.
+
+# 3. Define a function called is_vowel that takes a single character and outputs the boolean value True if the character is a vowel (a, e, i, o, u), and False otherwise.
+
+# 4. Define a function called sign_of_number that takes a number and outputs the sign of the number as a string.
+# The options for the sign of a number are Positive, Negative, and Zero (note capitalization of the first character).
