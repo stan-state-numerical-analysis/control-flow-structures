c36fe3684edad8dac67c297704075f87b41049f5
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-07 08:09:13 -0700

    Create task_4.py

---
+++ homework/task_4.py
@@ -0,0 +1,14 @@
+# Task: Create functions using combinations of conditional statements, for loops, and while loops.
+
+# 1. Define a function called is_prime that takes an integer input and outputs the boolean value True if the number is prime,
+# and False otherwise. To check if a number is prime, we only need to test if it is divisible by primes whose squares are less
+# than or equal to the number we are testing.
+
+
+
+# 2. Define a function called fibonacci that takes a positive integer n and outputs the nth Fibonacci number.
+
+
+
+# 3. Define a function called prime_fibonacci that takes a positive integer n and outputs a list of the first n prime Fibonacci numbers.
+# This function should call the previous two functions within it.
